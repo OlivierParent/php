0bda9cda0dd0b335588ad7c9ee0cfc637bd96497
Olivier Parent <olivier.parent@arteveldehs.be>
2013-10-01 16:35:07 +0200

    Update

---
+++ basis/operator/binair.php
@@ -0,0 +1,9 @@
+<?php
+$i = 0;
+$j = 0;
+
+$i = $i + 2;
+$j += 2;
+
+var_dump($i); // output: 2
+var_dump($j); // output: 2
---
+++ basis/operator/ternair.php
@@ -0,0 +1,4 @@
+<?php
+$bool = true;
+// Ternaire operator: de conditionele operator (? :)
+echo $bool ? 'WAAR' : 'ONWAAR'; // output: WAAR
