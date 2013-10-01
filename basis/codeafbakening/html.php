d84f89cb93bd5c9f2447f3d2d3414ee147647957
Olivier Parent <olivier.parent@arteveldehs.be>
2013-10-01 12:32:29 +0200

    Update

---
+++ basis/codeafbakening/html.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Dit is een HTML5-Document</title>
+</head>
+<body>
+<?php
+    // Hier komt PHP-code tussen de PHP-begintag en de PHP-eindtag
+?>
+</body>
+</html>
---
+++ basis/codeafbakening/snelschrift.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Dit is een HTML5-document</title>
+</head>
+<body>
+    <!-- Gewone echo -->
+    <p><?php echo 'Hallo wereld!'; ?></p>
+    <!-- Snelschriftversie -->
+    <p><?='Hallo wereld!' ?></p>
+</body>
+</html>
